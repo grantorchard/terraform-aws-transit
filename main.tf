9ec83c4547b10b95c11ee6d58e28c5c1ee5058bf
Grant Orchard <go@hashicorp.com>
2021-08-23 16:15:35 +1000

    remove hcp tg attachment for recreation

--- main.tf
+++ main.tf
@@ -44,26 +44,26 @@ resource "aws_ram_resource_association" "this" {
   resource_arn       = aws_ec2_transit_gateway.this.arn
 }
 
-resource "hcp_aws_transit_gateway_attachment" "this" {
-  depends_on = [
-    aws_ram_principal_association.this,
-    aws_ram_resource_association.this,
-  ]
+# resource "hcp_aws_transit_gateway_attachment" "this" {
+#   depends_on = [
+#     aws_ram_principal_association.this,
+#     aws_ram_resource_association.this,
+#   ]
 
-  hvn_id                        = local.hcp_hvn_id
-  transit_gateway_attachment_id = "hcp-tgw-attachment"
-  transit_gateway_id            = aws_ec2_transit_gateway.this.id
-  resource_share_arn            = aws_ram_resource_share.this.arn
-}
+#   hvn_id                        = local.hcp_hvn_id
+#   transit_gateway_attachment_id = "hcp-tgw-attachment"
+#   transit_gateway_id            = aws_ec2_transit_gateway.this.id
+#   resource_share_arn            = aws_ram_resource_share.this.arn
+# }
 
-resource "aws_ec2_transit_gateway_vpc_attachment_accepter" "this" {
-  transit_gateway_attachment_id = hcp_aws_transit_gateway_attachment.this.provider_transit_gateway_attachment_id
-}
+# resource "aws_ec2_transit_gateway_vpc_attachment_accepter" "this" {
+#   transit_gateway_attachment_id = hcp_aws_transit_gateway_attachment.this.provider_transit_gateway_attachment_id
+# }
 
-resource "hcp_hvn_route" "route" {
-	for_each = toset(var.aws_subnets)
-  hvn_link         = local.hcp_hvn_self_link
-  hvn_route_id     = "hcp-to-${replace(split("/", each.value)[0], ".", "-")}"
-  destination_cidr = each.value
-  target_link      = hcp_aws_transit_gateway_attachment.this.self_link
-}
+# resource "hcp_hvn_route" "route" {
+# 	for_each = toset(var.aws_subnets)
+#   hvn_link         = local.hcp_hvn_self_link
+#   hvn_route_id     = "hcp-to-${replace(split("/", each.value)[0], ".", "-")}"
+#   destination_cidr = each.value
+#   target_link      = hcp_aws_transit_gateway_attachment.this.self_link
+# }
