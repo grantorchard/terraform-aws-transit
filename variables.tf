7ba7d82740c4f40efddaa4fc45cd2e18395b033c
Grant Orchard <go@hashicorp.com>
2021-09-07 20:21:11 +1000

    add private subnets for route creation on HCP side

--- variables.tf
+++ variables.tf
@@ -1,14 +1,11 @@
-variable "tags" {
-	type = map
-	description = "A map of tags that will be merged with the "
-	default = {}
-}
-
 variable "aws_subnets" {
 	type = list(string)
 	default = [
 		"10.0.101.0/24",
     "10.0.102.0/24",
-    "10.0.103.0/24"
+    "10.0.103.0/24",
+		"10.0.1.0/24",
+		"10.0.2.0/24",
+		"10.0.3.0/24"
 	]
 }
