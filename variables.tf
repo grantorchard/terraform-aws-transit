0abe877d9def5de7dc738e788c63457669435bac
Jason Pilz <jasonpilz@gmail.com>
2021-10-28 11:20:16 -0700

    Terraform fmt

--- variables.tf
+++ variables.tf
@@ -1,18 +1,20 @@
 variable "aws_subnets" {
-	type = list(string)
-	default = [
-		"10.0.101.0/24",
+  type = list(string)
+
+  default = [
+    "10.0.101.0/24",
     "10.0.102.0/24",
     "10.0.103.0/24",
-		"10.0.1.0/24",
-		"10.0.2.0/24",
-		"10.0.3.0/24"
-	]
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24"
+  ]
 }
 
 variable "gcp_subnets" {
-	type = list(string)
-	default = [
-		"172.16.0.0/21"
-	]
+  type = list(string)
+
+  default = [
+    "172.16.0.0/21"
+  ]
 }
