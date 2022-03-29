214492033c68c85ff87a3dad0ca0a68fb017230d
Grant Orchard <go@hashicorp.com>
2022-03-29 12:57:50 +1100

    split out vault and consul security groups and add as outputs

--- main.tf
+++ main.tf
@@ -109,131 +109,272 @@ resource "hcp_hvn_route" "route" {
   target_link      = hcp_aws_transit_gateway_attachment.this.self_link
 }
 
-resource "aws_security_group" "hcp" {
-	name        = "hcp access"
-  description = "Permit access to Vault and Consul on HCP"
+resource "aws_security_group" "hcp_vault" {
+	name        = "hcp vault"
+  description = "Permit access to Vault on HCP"
   vpc_id      = local.vpc_id
-	egress = [
-    {
-			description = "vault"
-      from_port        = 8200
-      to_port          = 8200
-      protocol         = "tcp"
-      cidr_blocks      = ["172.25.16.0/20"]
-			ipv6_cidr_blocks = []
-			security_groups = []
-			prefix_list_ids = []
-			self = false
-    },
-		{
-			description = "consul"
-			from_port        = 8300
-      to_port          = 8300
-      protocol         = "tcp"
-      cidr_blocks      = ["172.25.16.0/20"]
-			ipv6_cidr_blocks = []
-			security_groups = []
-			prefix_list_ids = []
-			self = false
-		},
-		{
-			description = "consul"
-			from_port        = 8301
-      to_port          = 8301
-      protocol         = "tcp"
-      cidr_blocks      = ["172.25.16.0/20"]
-			ipv6_cidr_blocks = []
-			security_groups = []
-			prefix_list_ids = []
-			self = true
-		},
-		{
-			description = "consul"
-			from_port        = 8301
-      to_port          = 8301
-      protocol         = "udp"
-      cidr_blocks      = ["172.25.16.0/20"]
-			ipv6_cidr_blocks = []
-			security_groups = []
-			prefix_list_ids = []
-			self = true
-		},
-		{
-			description = "consul"
-			from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"
-      cidr_blocks      = ["172.25.16.0/20"]
-			ipv6_cidr_blocks = []
-			security_groups = []
-			prefix_list_ids = []
-			self = false
-		},
-		{
-			description = "consul"
-			from_port        = 443
-      to_port          = 443
-      protocol         = "tcp"
-      cidr_blocks      = ["172.25.16.0/20"]
-			ipv6_cidr_blocks = []
-			security_groups = []
-			prefix_list_ids = []
-			self = false
-		},
-		{
-			description = "consul"
-      from_port        = 53
-      to_port          = 53
-      protocol         = "udp"
-      cidr_blocks      = ["172.25.16.0/20"]
-			ipv6_cidr_blocks = []
-			security_groups = []
-			prefix_list_ids = []
-			self = false
-    }
-  ]
-	ingress = [
-		{
-			description = "consul"
-      from_port        = 8301
-      to_port          = 8301
-      protocol         = "udp"
-      cidr_blocks      = ["172.25.16.0/20"]
-			ipv6_cidr_blocks = []
-			security_groups = []
-			prefix_list_ids = []
-			self = true
-		},
-		{
-			description = "consul"
-      from_port        = 8301
-      to_port          = 8301
-      protocol         = "tcp"
-      cidr_blocks      = ["172.25.16.0/20"]
-			ipv6_cidr_blocks = []
-			security_groups = []
-			prefix_list_ids = []
-			self = true
-		}
-	]
 }
 
-resource "aws_security_group_rule" "allow_self" {
-  description       = "Allow members of this security group to communicate over all ports"
-  protocol          = "-1"
-  security_group_id = aws_security_group.hcp.id
-  self              = true
-  from_port         = 0
-  to_port           = 0
+resource "aws_security_group" "hcp_consul" {
+	name        = "hcp consul"
+  description = "Permit access to and from Consul on HCP"
+  vpc_id      = local.vpc_id
+}
+
+# resource "aws_security_group" "permit_self" {
+# 	name        = "permit self"
+#   description = "Permit access between members of this security group"
+#   vpc_id      = local.vpc_id
+# }
+
+# ## Rules for permit_self
+# resource "aws_security_group_rule" "allow_self_ingress" {
+#   description       = "Allow members of this security group ingress communication to each other over all ports"
+#   protocol          = "-1"
+#   security_group_id = aws_security_group.hcp.id
+#   self              = true
+#   from_port         = 0
+#   to_port           = 0
+#   type              = "ingress"
+# }
+
+# resource "aws_security_group_rule" "allow_self_egress" {
+#   description       = "Allow members of this security group egress communication to each other over all ports"
+#   protocol          = "-1"
+#   security_group_id = aws_security_group.hcp.id
+#   from_port         = 0
+#   to_port           = 0
+#   type              = "egress"
+# }
+
+## Rules for Consul
+resource "aws_security_group_rule" "allow_serf_udp_ingress" {
+  description       = "Allow serf access ingress over udp from HCP"
+  protocol          = "udp"
+  security_group_id = aws_security_group.hcp_consul.id
+	cidr_blocks       = ["172.25.16.0/20"]
+  from_port         = 8301
+  to_port           = 8301
   type              = "ingress"
 }
 
-resource "aws_security_group_rule" "allow_all_egress" {
-  description       = "Allow egress access to the Internet."
-  protocol          = "-1"
-  security_group_id = aws_security_group.hcp.id
-  cidr_blocks       = ["0.0.0.0/0"]
-  from_port         = 0
-  to_port           = 0
+resource "aws_security_group_rule" "allow_serf_tcp_ingress" {
+  description       = "Allow serf access ingress over tcp from HCP"
+  protocol          = "tcp"
+  security_group_id = aws_security_group.hcp_consul.id
+	cidr_blocks       = ["172.25.16.0/20"]
+  from_port         = 8301
+  to_port           = 8301
+  type              = "ingress"
+}
+
+resource "aws_security_group_rule" "allow_serf_udp_self_ingress" {
+  description       = "Allow members of this security group ingress communication serf over udp"
+  protocol          = "udp"
+  security_group_id = aws_security_group.hcp_consul.id
+	self  						= true
+  from_port         = 8301
+  to_port           = 8301
+  type              = "ingress"
+}
+
+resource "aws_security_group_rule" "allow_serf_tcp_self_ingress" {
+  description       = "Allow members of this security group ingress communication serf over tcp"
+  protocol          = "tcp"
+  security_group_id = aws_security_group.hcp_consul.id
+	self  						= true
+  from_port         = 8301
+  to_port           = 8301
+  type              = "ingress"
+}
+
+resource "aws_security_group_rule" "allow_serf_udp_self_egress" {
+  description       = "Allow members of this security group egress communication serf over udp"
+  protocol          = "udp"
+  security_group_id = aws_security_group.hcp_consul.id
+	self  						= true
+  from_port         = 8301
+  to_port           = 8301
   type              = "egress"
 }
+
+resource "aws_security_group_rule" "allow_serf_tcp_self_egress" {
+  description       = "Allow members of this security group egress communication serf over tcp"
+  protocol          = "tcp"
+  security_group_id = aws_security_group.hcp_consul.id
+	self  						= true
+  from_port         = 8301
+  to_port           = 8301
+  type              = "egress"
+}
+
+resource "aws_security_group_rule" "allow_http_egress" {
+  description       = "Allow members of this security group egress communication to HCP over http"
+  protocol          = "tcp"
+  security_group_id = aws_security_group.hcp_consul.id
+	cidr_blocks       = ["172.25.16.0/20"]
+  from_port         = 80
+  to_port           = 80
+  type              = "egress"
+}
+
+resource "aws_security_group_rule" "allow_https_egress" {
+  description       = "Allow members of this security group egress communication to HCP over https"
+  protocol          = "tcp"
+  security_group_id = aws_security_group.hcp_consul.id
+	cidr_blocks       = ["172.25.16.0/20"]
+  from_port         = 443
+  to_port           = 443
+  type              = "egress"
+}
+
+resource "aws_security_group_rule" "allow_consul_rpc_egress" {
+  description       = "Allow members of this security group egress communication to Consul over rpc"
+  protocol          = "tcp"
+  security_group_id = aws_security_group.hcp_consul.id
+	cidr_blocks       = ["172.25.16.0/20"]
+  from_port         = 8300
+  to_port           = 8300
+  type              = "egress"
+}
+
+resource "aws_security_group_rule" "allow_serf_udp_egress" {
+  description       = "Allow members of this security group egress communication to Consul serf over tcp"
+  protocol          = "tcp"
+  security_group_id = aws_security_group.hcp_consul.id
+	cidr_blocks       = ["172.25.16.0/20"]
+  from_port         = 8301
+  to_port           = 8301
+  type              = "egress"
+}
+
+resource "aws_security_group_rule" "allow_serf_tcp_egress" {
+  description       = "Allow members of this security group egress communication to Consul serf over udp"
+  protocol          = "udp"
+  security_group_id = aws_security_group.hcp_consul.id
+	cidr_blocks       = ["172.25.16.0/20"]
+  from_port         = 8301
+  to_port           = 8301
+  type              = "egress"
+}
+
+## Vault Rules
+resource "aws_security_group_rule" "allow_vault_egress" {
+  description       = "Allow members of this security group egress communication to Consul serf over udp"
+  protocol          = "udp"
+  security_group_id = aws_security_group.hcp_vault.id
+	cidr_blocks       = ["172.25.16.0/20"]
+  from_port         = 8200
+  to_port           = 8200
+  type              = "egress"
+}
+
+
+# resource "aws_security_group" "hcp" {
+# 	name        = "hcp access"
+#   description = "Permit access to Vault and Consul on HCP"
+#   vpc_id      = local.vpc_id
+# 	egress = [
+#     {
+# 			description = "vault"
+#       from_port        = 8200
+#       to_port          = 8200
+#       protocol         = "tcp"
+#       cidr_blocks      = ["172.25.16.0/20"]
+# 			ipv6_cidr_blocks = []
+# 			security_groups = []
+# 			prefix_list_ids = []
+# 			self = false
+#     },
+# 		{
+# 			description = "consul"
+# 			from_port        = 8300
+#       to_port          = 8300
+#       protocol         = "tcp"
+#       cidr_blocks      = ["172.25.16.0/20"]
+# 			ipv6_cidr_blocks = []
+# 			security_groups = []
+# 			prefix_list_ids = []
+# 			self = false
+# 		},
+# 		{
+# 			description = "consul"
+# 			from_port        = 8301
+#       to_port          = 8301
+#       protocol         = "tcp"
+#       cidr_blocks      = ["172.25.16.0/20"]
+# 			ipv6_cidr_blocks = []
+# 			security_groups = []
+# 			prefix_list_ids = []
+# 			self = true
+# 		},
+# 		{
+# 			description = "consul"
+# 			from_port        = 8301
+#       to_port          = 8301
+#       protocol         = "udp"
+#       cidr_blocks      = ["172.25.16.0/20"]
+# 			ipv6_cidr_blocks = []
+# 			security_groups = []
+# 			prefix_list_ids = []
+# 			self = true
+# 		},
+# 		{
+# 			description = "consul"
+# 			from_port        = 80
+#       to_port          = 80
+#       protocol         = "tcp"
+#       cidr_blocks      = ["172.25.16.0/20"]
+# 			ipv6_cidr_blocks = []
+# 			security_groups = []
+# 			prefix_list_ids = []
+# 			self = false
+# 		},
+# 		{
+# 			description = "consul"
+# 			from_port        = 443
+#       to_port          = 443
+#       protocol         = "tcp"
+#       cidr_blocks      = ["172.25.16.0/20"]
+# 			ipv6_cidr_blocks = []
+# 			security_groups = []
+# 			prefix_list_ids = []
+# 			self = false
+# 		},
+# 		{
+# 			description = "consul"
+#       from_port        = 53
+#       to_port          = 53
+#       protocol         = "udp"
+#       cidr_blocks      = ["172.25.16.0/20"]
+# 			ipv6_cidr_blocks = []
+# 			security_groups = []
+# 			prefix_list_ids = []
+# 			self = false
+#     }
+#   ]
+# 	ingress = [
+# 		{
+# 			description = "consul"
+#       from_port        = 8301
+#       to_port          = 8301
+#       protocol         = "udp"
+#       cidr_blocks      = ["172.25.16.0/20"]
+# 			ipv6_cidr_blocks = []
+# 			security_groups = []
+# 			prefix_list_ids = []
+# 			self = true
+# 		},
+# 		{
+# 			description = "consul"
+#       from_port        = 8301
+#       to_port          = 8301
+#       protocol         = "tcp"
+#       cidr_blocks      = ["172.25.16.0/20"]
+# 			ipv6_cidr_blocks = []
+# 			security_groups = []
+# 			prefix_list_ids = []
+# 			self = true
+# 		}
+# 	]
+# }
